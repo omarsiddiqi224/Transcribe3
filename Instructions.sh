46a3e87400a185690c089268fb76a80982d22389
omarsiddiqi224 <103967116+omarsiddiqi224@users.noreply.github.com>
2023-12-15 01:06:27 -0500

    Update Instructions.sh

--- Instructions.sh
+++ Instructions.sh
@@ -5,6 +5,7 @@ source env/bin/activate
 
 pip install -r requirements.txt 
 pipx install insanely-fast-whisper==0.0.13 --force
+pipx runpip insanely-fast-whisper install wheel
 pipx runpip insanely-fast-whisper install flash-attn --no-build-isolation
 python -m pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
 pip install --upgrade transformers optimum accelerate
