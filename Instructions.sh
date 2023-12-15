46b69cdd49bbe447fe26458aceaf8dec2ed75d7f
omarsiddiqi224 <103967116+omarsiddiqi224@users.noreply.github.com>
2023-12-15 00:00:57 -0500

    Update Instructions.sh

--- Instructions.sh
+++ Instructions.sh
@@ -1,8 +1,9 @@
-python -m venv env
+#!/bin/bash
+
+python3 -m venv env
 source env/bin/activate
 
-pip install requirements.txt 
+pip install -r requirements.txt 
 pipx install insanely-fast-whisper
 pipx runpip insanely-fast-whisper install flash-attn --no-build-isolation
 python -m pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
-pip install --upgrade transformers optimum accelerate
